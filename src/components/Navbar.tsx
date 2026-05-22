159b6e5a5e26f14893667ffe27b86cc2726dbe7e
Luiz Mujalli <lhmujalli@hotmail.com>
2026-05-22 20:40:43 -0300

    fix: changes on navbar.tsx and ebook.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,7 +68,7 @@ const Navbar = () => {
             >
               <span className="leading-none">Gedeon Monteiro</span>
               <span className="hidden min-[901px]:block w-[1px] h-4 bg-gray-400 mx-3 self-center"></span>
-              <span className="font-light text-gray-500 text-base sm:text-lg min-[901px]:text-lg uppercase tracking-widest min-[901px]:normal-case min-[901px]:tracking-normal leading-none min-[901px]:mt-[2px]">
+              <span className="font-light text-gray-500 text-base sm:text-lg min-[901px]:text-lg tracking-tight min-[901px]:normal-case min-[901px]:tracking-normal leading-none min-[901px]:mt-[2px]">
                 Terapia Energética e Espiritual
               </span>
             </Link>
